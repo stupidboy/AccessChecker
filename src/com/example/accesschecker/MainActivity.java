82261d0a8ec4281da2af547f75d9c5ab9f3cc682
stupidboy <316981665@qq.com>
2015-05-06 15:29:33 +0800

    update
    1. mix mode
    2. draw lines ponits

--- src/com/example/accesschecker/MainActivity.java
+++ src/com/example/accesschecker/MainActivity.java
@@ -4,35 +4,71 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-    SimpleCheckerView mChecker ;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mChecker = (SimpleCheckerView)this.findViewById(R.id.textView1);
-        
-    }
+	SimpleCheckerView mChecker;
+	Button mButonRegen, mButtonCheck;
+	EditText mEditText ;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mChecker = (SimpleCheckerView) this.findViewById(R.id.textView1);
+		mButonRegen = (Button) this.findViewById(R.id.button1);
+		mEditText = (EditText)this.findViewById(R.id.editText1);
+		mButonRegen.setOnClickListener(new OnClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			@Override
+			public void onClick(View arg0) {
+				if (mChecker != null) {
+					mChecker.genCheckSource();
+				}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+			}
+
+		});
+		mButtonCheck = (Button) this.findViewById(R.id.button2);
+		mButtonCheck.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				String value = "";
+				value = mEditText.getText().toString();
+			    if(mChecker != null){
+			    	if( mChecker.getCheckResult(value)){
+			    		finish();
+			    	}
+			       
+			    }
+				
+			}
+			
+			
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- src/com/example/accesschecker/SimpleCheckerView.java
+++ src/com/example/accesschecker/SimpleCheckerView.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.widget.Toast;
 
 public class SimpleCheckerView extends CheckerView {
 	Context mContext = null;
@@ -16,22 +17,27 @@ public class SimpleCheckerView extends CheckerView {
 	private static final int CONFIG_TYPE_INT = 0;
 	private static final int CONFIG_TYPE_CHAR = 1;
 	private static final int CONFIG_TYPE_MIX = 2;
+	private static final int CONFIG_LINES = 2;
+	private static final int CONFIG_POINTS = 40;
+	private static final int CONFIG_CASE = 0;
+	int mType ,mCaseSen = 0;
+	Paint mPaint;
 
-	int mType = 0;
-    Paint mPaint ;
-    void init(Context context){
-    	
+	void init(Context context) {
+
 		mContext = context;
-		mType = CONFIG_TYPE_CHAR;
+		mType = CONFIG_TYPE_MIX;
+		mCaseSen = CONFIG_CASE;
 		genCheckSource();
 		mPaint = new Paint();
 		mPaint.setAntiAlias(true);
 		mPaint.setTextSize(30);
 		mPaint.setStrokeWidth(3);
-    	
-    }
+
+	}
+
 	public SimpleCheckerView(Context context) {
-		
+
 		super(context);
 		init(context);
 	}
@@ -51,16 +57,29 @@ public class SimpleCheckerView extends CheckerView {
 
 	@Override
 	public boolean getCheckResult(String value) {
+		boolean result = false;
 		if (value != null) {
-			return value.equals(mRamdomResult);
+			if (mCaseSen == 1) {
+				result = value.equals(mRamdomResult);
+			} else {
+				result = value.equalsIgnoreCase(mRamdomResult);
+
+			}
+
 		}
-		return false;
+
+		Toast.makeText(mContext, result ? "done" : "try again",
+				Toast.LENGTH_SHORT).show();
+		if (!result) {
+			this.genCheckSource();
+		}
+		return result;
 	}
 
 	char getNextChar() {
 		Random r = new Random();
-		boolean lowerCase =  r.nextInt(2) == 1 ;
-		Log.e("xx","case..."+lowerCase);
+		boolean lowerCase = r.nextInt(5) == 2;
+		Log.e("xx", "case..." + lowerCase);
 		if (lowerCase) {
 			return (char) (r.nextInt(26) + 65);
 		} else {
@@ -73,11 +92,16 @@ public class SimpleCheckerView extends CheckerView {
 		return r.nextInt(10);
 	}
 
+	int getNextOffset() {
+		Random r = new Random();
+		return r.nextInt() % 20;
+	}
+
 	@Override
 	void genCheckSource() {
 		// gen Check Source ramdom int;
-		Log.d("xx","gen.....");
-		mRamdomResult= "";
+		Log.d("xx", "gen.....");
+		mRamdomResult = "";
 		switch (mType) {
 		case CONFIG_TYPE_INT:
 			for (int i = 0; i < CONFIG_BITS; i++) {
@@ -91,16 +115,37 @@ public class SimpleCheckerView extends CheckerView {
 			break;
 		case CONFIG_TYPE_MIX:
 			Random r = new Random();
-			if (r.nextInt(2) == 1) {
-				mRamdomResult += getNextInt();
-			} else {
-				mRamdomResult += getNextChar();
+			for (int i = 0; i < CONFIG_BITS; i++) {
+				if (r.nextInt(2) == 1) {
+					mRamdomResult += getNextInt();
+				} else {
+					mRamdomResult += getNextChar();
+				}
 			}
 			break;
 		default:
 			break;
 		}
-		Log.d("xx","gen.....done  "+mRamdomResult);
+		Log.d("xx", "gen.....done  " + mRamdomResult);
+		this.invalidate();
+	}
+
+	void drawPoints(Canvas canvas) {
+		Random r = new Random();
+		for (int i = 0; i < CONFIG_POINTS; i++) {
+			int x = r.nextInt(getWidth());
+			int y = r.nextInt(getHeight());
+			canvas.drawPoint(x, y, mPaint);
+		}
+	}
+
+	void drawLine(Canvas canvas) {
+		Random r = new Random();
+		for (int i = 0; i < CONFIG_LINES; i++) {
+			int y1 = r.nextInt(this.getHeight());
+			int y2 = r.nextInt(this.getHeight());
+			canvas.drawLine(0, y1, this.getWidth(), y2, mPaint);
+		}
 	}
 
 	@Override
@@ -108,19 +153,24 @@ public class SimpleCheckerView extends CheckerView {
 		canvas.drawColor(Color.BLUE);
 		int height = this.getHeight();
 		int width = this.getWidth();
-		
-		//--------------------------------
-		//--A---B---C---D---E---F--------
-		//-------------------------------
+
+		// --------------------------------
+		// --A---B---C---D---E---F--------
+		// -------------------------------
 		//
-		if(mRamdomResult == null){
-			genCheckSource();
+		// if(mRamdomResult == null){
+		// genCheckSource();
+		// }
+		char[] random = mRamdomResult.toCharArray();
+		for (int i = 0; i < CONFIG_BITS; i++) {
+			Log.e("xx", "draw...." + random[i]);
+			canvas.drawText("" + random[i], 10 + 60 * (i),
+					60 + getNextOffset(), mPaint);
 		}
-		char[] random= mRamdomResult.toCharArray();
-		for(int i = 0 ;i<CONFIG_BITS;i++){
-		    Log.e("xx","draw...."+random[i]);
-		   canvas.drawText(""+random[i], 10+60*(i),60/* -2*getNextInt()*/, mPaint);
-		}
+		// draw line
+
+		drawLine(canvas);
+		drawPoints(canvas);
 	}
 
 }
